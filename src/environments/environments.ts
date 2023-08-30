2798adba2d12070c0dcc13a2919a1db16178635c
Renzo <renzofab2@hotmail.com>
2023-08-30 14:41:41 -0500

    Corrigiendo Estilos

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,4 +1,4 @@
 export const environments = {
-  baseUrl: 'http://localhost:2400',
-  // baseUrl: 'https://centrogaming-back.onrender.com',
+  // baseUrl: 'http://localhost:2400',
+  baseUrl: 'https://centrogaming-back.onrender.com',
 };
